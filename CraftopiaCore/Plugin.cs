df27da7b9bac3cba950e97d75d7bc40983bbd476
デンパ時計 <jsoldenpa@yahoo.co.jp>
2020-11-07 02:45:02 +0900

    プロジェクト ファイルを追加します。

---
+++ CraftopiaCore/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftopiaCore
+{
+    [BepInPlugin("com.github.denpadokei.CraftopiaCore", "CraftpiaCore", "0.0.0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance { get; private set; }
+
+        /// <summary>
+        /// 最初に読み込まれます。
+        /// </summary>
+        void Awake()
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
